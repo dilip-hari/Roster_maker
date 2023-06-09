5d1001020233f05868c77b454aed68c752b4810b
dilip-hari <54951927+dilip-hari@users.noreply.github.com>
2023-06-09 11:19:24 +0530

    Add files via upload

---
+++ Roster_maker.py
@@ -0,0 +1,27 @@
+# import module
+import openpyxl
+from openpyxl.styles import PatternFill
+from openpyxl.styles.colors import Color
+
+# load excel with its path
+wrkbk = openpyxl.load_workbook("roster.xlsx")
+
+sh = wrkbk.active
+
+fill_cell1 = PatternFill(patternType='solid', fgColor='FFFF00')
+fill_cell2 = PatternFill(patternType='solid', fgColor='ff0000')
+
+# iterate through excel and display data
+for i in range(1, sh.max_row+1):
+	for j in range(1, sh.max_column+1):
+		cell_obj = sh.cell(row=i, column=j)
+		if cell_obj.value=='as':
+		    cell_obj.value='shift1'
+		    cell_obj.fill = fill_cell1
+		elif cell_obj.value=='ns':
+		    cell_obj.value='shift2'
+		    cell_obj.fill = fill_cell2
+		print(cell_obj.value)
+wrkbk.save("roster1.xlsx")
+		    
+
